07849a8e2ac440c921f7af07213d9c5a43a142c0
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-22 08:20:07 +0000

    Initial commit.

---
+++ app.php
@@ -0,0 +1,12 @@
+<!-- Make use of some of the PHP loop shorthand to create HTML for a restaurant menu. Practice using the different loop types with HTML. -->
+
+<h1>Welcome to the Repetitive Cafe</h1>
+
+<h3>Drinks!</h3>
+<ul>
+
+</ul>
+<h3>Pastries! ($2 each)</h3>
+<ul>
+
+</ul>
